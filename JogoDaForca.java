935e9e8a94c1dc374987442fce6d1cee3652a4f0
Guilherme Roxo <guilhermegmaf@gmail.com>
2026-03-22 14:35:12 -0300

    feat: adiciona classe JogoDaForca com seus atributos padrão

---
+++ JogoDaForca.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class JogoDaForca {
+
+    private String palavra;
+    private String dica;
+    private int acertos;
+    private int codPenalidade;
+    private String nomePenalidade;
+    private String resultado = "em andamento";
+    private ArrayList<String> palavrasAnteriores;
+    private ArrayList<String> ocorrencias;
+}
